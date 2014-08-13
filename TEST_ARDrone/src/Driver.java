831882acba834ad16ac457d8c30e9f80ade143b4
rseljeii <rseljeii@yahoo.com>
2014-08-12 23:42:50 -0600

    Update

--- TEST_ARDrone/src/Driver.java
+++ TEST_ARDrone/src/Driver.java
@@ -1,3 +1,6 @@
+//import com.codeminders.ardrone.ARDrone;
+import java.util.Scanner;
+
 import com.codeminders.ardrone.ARDrone;
 
 public class Driver
@@ -10,7 +13,11 @@ public class Driver
      */
     public static void main(String[] args)
     {
-        ARDrone drone;
+    	Scanner scan = new Scanner (System.in);
+    	int a;
+    	ARDrone drone;
+    	
+    	
         try
         {
             // Create ARDrone object,
@@ -34,9 +41,60 @@ public class Driver
             drone.takeOff();
 
             // Fly a little :)
-            Thread.sleep(2000);
+            Thread.sleep(10000);
+            
+            //Move Forward
+            
+            System.out.println("Forward");
+            for(int i = 0; i < 100000; i++)
+            drone.move(0, -0.9f, 0, 0);
+            Thread.sleep(3000);
+            
+			//Turn Left
+            System.out.println("Left");
+            for(int i = 0; i < 500000; i++)
+            drone.move(0, 0, 0, -0.9f);
+            Thread.sleep(3000);
+            
+            // Turn Right
+            System.out.println("Right");
+            for(int i = 0; i < 500000; i++)
+            	drone.move(0, 0, 0, 0.9f);
+            Thread.sleep(3000);
+           
+ 			//Move Backward
+            System.out.println("Backward");
+            for(int i = 0; i < 1000000; i++)
+            drone.move(0, 0.9f, 0, 0);
+            Thread.sleep(3000);
 
-            // Land
+          
+           
+
+            /*
+ 
+            System.out.println("Command");
+            a = scan.nextInt();
+            int i = 0;
+            Boolean temp = true;
+            
+            while(temp)
+            {
+            	System.out.print("\nEnter Command " + i++ + ": ");
+            	if(a == 5)
+            		drone.move(0, 0, 0, -0.9f);
+            	if(a != 5)
+            		temp = false;
+            }
+
+
+			*/
+
+
+
+
+
+			// Land
             System.out.println("Landing");
             //System.err.println("Landing");
             drone.land();
@@ -51,5 +109,6 @@ public class Driver
         {
             e.printStackTrace();
         }
+                
     }
 }
